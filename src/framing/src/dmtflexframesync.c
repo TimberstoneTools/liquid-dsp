1c4e657f2ec7a722d0325de7f64036754ea78563
Darryl Gamroth <dgamroth@timberstonetools.com>
2016-03-16 13:58:02 -0700

    dmtflexframesync: don't save payload symbols unless DEBUG_DMTFLEXFRAMESYNC == 1

--- src/framing/src/dmtflexframesync.c
+++ src/framing/src/dmtflexframesync.c
@@ -102,7 +102,9 @@ struct dmtflexframesync_s {
     unsigned int payload_enc_len;       // length of encoded payload
     unsigned int payload_mod_len;       // number of payload modem symbols
     int payload_valid;                  // valid payload flag
+#if DEBUG_DMTFLEXFRAMESYNC
     float complex * payload_syms;       // received payload symbols
+#endif
 
     // callback
     framesync_callback callback;        // user-defined callback function
@@ -197,7 +199,9 @@ dmtflexframesync dmtflexframesync_create(unsigned int       _M,
     q->payload_enc_len = packetizer_get_enc_msg_len(q->p_payload);
     q->payload_enc = (unsigned char*) malloc(q->payload_enc_len*sizeof(unsigned char));
     q->payload_dec = (unsigned char*) malloc(q->payload_len*sizeof(unsigned char));
+#if DEBUG_DMTFLEXFRAMESYNC
     q->payload_syms = (float complex *) malloc(q->payload_len*sizeof(float complex));
+#endif
     q->payload_mod_len = 0;
 
     // reset state
@@ -220,7 +224,9 @@ void dmtflexframesync_destroy(dmtflexframesync _q)
     free(_q->p);
     free(_q->payload_enc);
     free(_q->payload_dec);
+#if DEBUG_DMTFLEXFRAMESYNC
     free(_q->payload_syms);
+#endif
 
     // free main object memory
     free(_q);
@@ -569,8 +575,8 @@ void dmtflexframesync_decode_header(dmtflexframesync _q)
         // re-compute number of modulated payload symbols
         div_t d = div(8*_q->payload_enc_len, _q->bps_payload);
         _q->payload_mod_len = d.quot + (d.rem ? 1 : 0);
-        _q->payload_syms = (float complex*) realloc(_q->payload_syms, _q->payload_mod_len*sizeof(float complex));
 #if DEBUG_DMTFLEXFRAMESYNC
+        _q->payload_syms = (float complex*) realloc(_q->payload_syms, _q->payload_mod_len*sizeof(float complex));
         printf("      * payload mod syms:   %u symbols\n", _q->payload_mod_len);
 #endif
     }
@@ -593,8 +599,10 @@ void dmtflexframesync_rxpayload(dmtflexframesync _q,
             unsigned int sym;
             modem_demodulate(_q->mod_payload, _X[i], &sym);
 
+#if DEBUG_DMTFLEXFRAMESYNC
             // store received symbol
             _q->payload_syms[_q->payload_symbol_index] = _X[i];
+#endif
 
             // pack decoded symbol into array
             liquid_pack_array(_q->payload_enc,
@@ -627,8 +635,13 @@ void dmtflexframesync_rxpayload(dmtflexframesync _q,
                 // set framestats internals
                 _q->framestats.rssi             = ofdmframesync_get_rssi(_q->fs);
                 _q->framestats.cfo              = ofdmframesync_get_cfo(_q->fs);
+#if DEBUG_DMTFLEXFRAMESYNC
                 _q->framestats.framesyms        = _q->payload_syms;
                 _q->framestats.num_framesyms    = _q->payload_mod_len;
+#else
+                _q->framestats.framesyms        = NULL;
+                _q->framestats.num_framesyms    = 0;
+#endif
                 _q->framestats.mod_scheme       = _q->ms_payload;
                 _q->framestats.mod_bps          = _q->bps_payload;
                 _q->framestats.check            = _q->check;
